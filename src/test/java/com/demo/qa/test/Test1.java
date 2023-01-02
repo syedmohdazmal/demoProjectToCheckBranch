4fe0fdad51249e65d89f1b6bd3b19fa2537643b6
azmal <syedmohdazmal@gmail.com>
2023-01-02 18:15:52 +0530

    test1 class added only

---
+++ src/test/java/com/demo/qa/test/Test1.java
@@ -0,0 +1,33 @@
+package com.demo.qa.test;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+import com.demo.qa.page.ElementsPage;
+
+public class Test1 extends BaseTest{
+	public static Logger logger=Logger.getLogger("ElementsTest.class");
+	public Test1() throws IOException {
+		super();
+	}
+	@Test(priority=1)
+	public void testRegistrationForm()
+	{
+		ElementsPage elePage=new ElementsPage(driver);
+		elePage.clickOnFirstNameText();
+		elePage.setFirstName();
+		elePage.clickOnLastText();
+		elePage.setLatName();
+		elePage.clicOnAddress();
+		elePage.setAddress();
+		elePage.clickOnEmailText();
+		elePage.setEmail();
+		elePage.clickOnPhoneText();
+		elePage.setPhone();
+		elePage.clickOnGender();
+		elePage.clickOnHobbies();
+	}
+
+}
